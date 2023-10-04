fc55ec2b3e48a9e3319851b58d3be5d488233b9f
Yanick Schulz <yanick.gaming94@gmail.com>
2023-10-04 12:22:35 +0200

    Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "dashboard" ,component: DashboardComponent},
+  {path: "" ,component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
