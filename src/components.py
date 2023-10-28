18c4fe4a56b80a19fdf3b5592363ecf82b8b296a
Yanick Schulz <yanick.schulz94@gmail.com>
2023-10-28 12:42:16 +0200

    erste version von der neuen Python Struktur

---
+++ src/components.py
@@ -0,0 +1,8 @@
+# Basic Components
+from src.app.components.basic.sidebar import sidebar_component
+from src.app.components.basic.titlebar import titlebar_component
+
+# Normal Components
+from src.app.components.home import home_component
+from src.app.components.testing import testing_component
+from src.app.components.dashboard import dashboard_component
